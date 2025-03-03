cc3aa9bef6bab462221e5ecf4d2400b508725ccf
Maimai10808 <331686549@qq.com>
2025-03-03 18:09:09 +0800

    Initial Commit

---
+++ Weather-appTests/Weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appTests.swift
+//  Weather-appTests
+//
+//  Created by mac on 3/3/25.
+//
+
+import Testing
+@testable import Weather_app
+
+struct Weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
